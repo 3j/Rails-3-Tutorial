dc9ed87cd81c45eda1b1353086b2f626e7f1f502
David G Chaves <davidgchaves@gmail.com>
2012-06-27 10:38:19 +0200

    Finish static contact page from exercise 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "Static pages" do
       page.should have_selector 'title', text: 'RoR Tutorial App | About Us'
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'h1', text: 'Contact'
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'title', text: 'RoR Tutorial App | Contact'
+    end
+  end
 end
