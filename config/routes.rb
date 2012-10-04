ee69969daeac7fa9b34f262f68a526ab0c5c8e5d
David G Chaves <davidgchaves@gmail.com>
2012-10-04 13:38:13 +0200

    Finish layout and routes adding skeleton and route for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails3Tutorial::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
