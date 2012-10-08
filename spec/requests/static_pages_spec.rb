8cac7eb51a3ac67d6cea023c15d1935cb65215bf
David G Chaves <davidgchaves@gmail.com>
2012-10-08 13:12:48 +0200

    Add test to check that the links on the layout go to the right pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe "Static pages" do
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title("About")
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title("Contact")
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title("Sign up")
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title("")
+  end
+
   shared_examples_for "all static pages" do
     it { should have_selector 'h1', text: heading }
     it { should have_css 'title', text: full_title(page_title) }
