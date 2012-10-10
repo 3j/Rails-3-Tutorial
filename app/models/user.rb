d06c5d5e55e4d54e006ef7b3d4e7f4e94a7f15fc
David G Chaves <davidgchaves@gmail.com>
2012-10-10 11:31:45 +0200

    Add spec to check User email saving as lower-case

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
-  before_save { |user| user.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email addresses with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it {should_not be_valid }
