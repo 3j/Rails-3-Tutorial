bc21a928ae0394d98a61864d21af9f0af79e1aa6
David G Chaves <davidgchaves@gmail.com>
2012-10-13 11:47:55 +0200

    Add a working User Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails3Tutorial::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
+describe "Profile Pages" do
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create :user }
+    before { visit user_path user }
+
+    it { should have_selector 'h1', text: user.name }
+    it { should have_selector 'title', text: user.name }
+  end
+end
+
 describe "User Pages" do
   subject { page }
-  let(:base_title) { "RoR Tutorial App" }
 
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector 'h1', text: 'Sign up' }
-    it { should have_selector 'title', text: "#{base_title} | Sign up" }
+    it { should have_selector 'title', text: "Sign up" }
   end
 end
