d9ce5f5f3b9c15d21c6f3af56d2bd99715a84400
David G Chaves <davidgchaves@gmail.com>
2012-10-23 10:47:14 +0200

    Add Micropost model and relationship with Users

---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content 
+  belongs_to :user
+
+  validates :user_id, presence: true 
+end
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Micropost do
+
+  let(:user) { FactoryGirl.create :user }
+
+  before do
+    @micropost = user.microposts.build content: "Lorem ipsum"
+  end
+
+  subject { @micropost }
+
+  it { should respond_to :content }
+  it { should respond_to :user_id }
+  it { should respond_to :user }
+
+  it { should be_valid }
+
+  it "should belong to the right user" do
+    expect(@micropost.user).to eq user
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to user_id" do
+      expect { Micropost.new user_id: user.id }.
+        to raise_error ActiveModel::MassAssignmentSecurity::Error
+    end
+  end
+  
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+end
