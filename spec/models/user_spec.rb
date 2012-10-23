83deebabd000a6d0d27f34cc964ff3b04daa5e8e
David G Chaves <davidgchaves@gmail.com>
2012-10-23 11:10:07 +0200

    Add ordering to user's microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -132,4 +132,18 @@ describe User do
       @user.remember_token.should_not be_blank
     end
   end
+
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create :micropost, user: @user, created_at: 1.day.ago
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create :micropost, user: @user, created_at: 1.hour.ago
+    end
+    
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
