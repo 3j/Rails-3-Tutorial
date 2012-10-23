24f177c40f053c84621ed8a7d832813bfb2de395
David G Chaves <davidgchaves@gmail.com>
2012-10-23 11:24:06 +0200

    Add delete user's microposts when the user is deleted

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -145,5 +145,15 @@ describe User do
     it "should have the right microposts in the right order" do
       expect(@user.microposts).to eq [newer_micropost, older_micropost]
     end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      expect(microposts).not_to be_empty # safety check in case dup be removed.
+
+      microposts.each do |micropost|
+        expect(Micropost.find_by_id micropost.id).to be_nil
+      end
+    end
   end
 end
