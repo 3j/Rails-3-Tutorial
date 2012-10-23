8e4c50d9c3ecf60aa430d014203085fb74334002
David G Chaves <davidgchaves@gmail.com>
2012-10-23 11:33:10 +0200

    Add micropost's content validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -31,4 +31,14 @@ describe Micropost do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
+
+  describe "with blank content" do
+    before { @micropost.content = '' }
+    it { should_not be_valid }
+  end
+
+  describe "with too long content" do
+    before { @micropost.content = 'a' * 141 }
+    it { should_not be_valid }
+  end
 end
