b31c2da0ea56fa4f2fa2eda1df1e7137ae1a3b62
David G Chaves <davidgchaves@gmail.com>
2012-10-25 11:49:33 +0200

    Add followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token 
@@ -14,6 +15,18 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false } 
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def following?(other_user)
+    relationships.find_by_followed_id other_user.id
+  end
+
+  def follow!(other_user)
+    relationships.create! followed_id: other_user.id
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
   def proto_feed
     microposts
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,10 @@ describe User do
   it { should respond_to :microposts } 
   it { should respond_to :proto_feed }
   it { should respond_to :relationships }
+  it { should respond_to :followed_users }
+  it { should respond_to :following? }
+  it { should respond_to :follow! }
+  it { should respond_to :unfollow! }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -166,6 +170,23 @@ describe User do
       its(:proto_feed) { should include older_micropost }
       its(:proto_feed) { should include newer_micropost }
       its(:proto_feed) { should_not include unfollowed_post }
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create :user }
+    before do
+      user.save
+      user.follow! other_user
+    end
+
+    it { should be_following other_user }
+    its(:followed_users) { should include other_user }
+
+    describe "and unfollowing" do
+      before { user.unfollow! other_user }
+      it { should_not be_following other_user }
+      its(:followed_users) { should_not include other_user }
     end
   end
 end
