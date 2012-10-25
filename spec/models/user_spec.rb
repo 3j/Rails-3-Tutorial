ba01876391e13b64227861db5618945751dcd2bc
David G Chaves <davidgchaves@gmail.com>
2012-10-25 10:36:06 +0200

    Use let to define user subject in user_spec in preference to @user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe User do
 
-  before do
-    @user = User.new name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar"
+  let(:user) do
+    User.new name: "Example User", email: "user@example.com",
+             password: "foobar", password_confirmation: "foobar"
   end
   
-  subject { @user }
+  subject { user }
 
   it { should respond_to :name }
   it { should respond_to :email }
@@ -31,17 +31,17 @@ describe User do
   end
 
   describe "when name is not present" do
-    before { @user.name = " " }
+    before { user.name = " " }
     it { should_not be_valid }
   end
 
   describe "when name is too long" do
-    before { @user.name = "a" * 51 }
+    before { user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
   describe "when email is not present" do
-    before { @user.email = " " }
+    before { user.email = " " }
     it { should_not be_valid }
   end
 
@@ -49,8 +49,8 @@ describe User do
     it "should be invalid" do
       invalid_addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       invalid_addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
+        user.email = invalid_address
+        user.should_not be_valid
       end
     end
   end
@@ -59,16 +59,16 @@ describe User do
     it "should be valid" do
       valid_addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       valid_addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
+        user.email = valid_address
+        user.should be_valid
       end
     end
   end
 
   describe "when email address is already taken" do
     before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
+      user_with_same_email = user.dup
+      user_with_same_email.email = user.email.upcase
       user_with_same_email.save
     end
     
@@ -79,39 +79,39 @@ describe User do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
     it "should be saved as all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      @user.reload.email.should eq mixed_case_email.downcase
+      user.email = mixed_case_email
+      user.save
+      user.reload.email.should eq mixed_case_email.downcase
     end
   end
 
   describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
+    before { user.password = user.password_confirmation = " " }
     it {should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
+    before { user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
   describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
+    before { user.password_confirmation = nil }
     it { should_not be_valid }
   end
 
   describe "when password is too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
+    before { user.password = user.password_confirmation = "a" * 5 }
     it { should_not be_valid }
   end
 
   describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email @user.email }
+    before { user.save }
+    let(:found_user) { User.find_by_email user.email }
 
     describe "with valid password" do
       it "should be the authenticated user" do
-        @user.should eq found_user.authenticate(@user.password)
+        user.should eq found_user.authenticate(user.password)
       end
     end
 
@@ -119,7 +119,7 @@ describe User do
       let(:user_for_invalid_password) { found_user.authenticate "invalid" }
       
       it "should not be the authenticated user" do
-        @user.should_not eq user_for_invalid_password
+        user.should_not eq user_for_invalid_password
       end
 
       specify { user_for_invalid_password.should be_false }
@@ -127,29 +127,29 @@ describe User do
   end
 
   describe "remember token" do
-    before { @user.save }
+    before { user.save }
     
     it "should not be blank" do
-      @user.remember_token.should_not be_blank
+      user.remember_token.should_not be_blank
     end
   end
 
   describe "micropost associations" do
-    before { @user.save }
+    before { user.save }
     let!(:older_micropost) do
-      FactoryGirl.create :micropost, user: @user, created_at: 1.day.ago
+      FactoryGirl.create :micropost, user: user, created_at: 1.day.ago
     end
     let!(:newer_micropost) do
-      FactoryGirl.create :micropost, user: @user, created_at: 1.hour.ago
+      FactoryGirl.create :micropost, user: user, created_at: 1.hour.ago
     end
     
     it "should have the right microposts in the right order" do
-      expect(@user.microposts).to eq [newer_micropost, older_micropost]
+      expect(user.microposts).to eq [newer_micropost, older_micropost]
     end
 
     it "should destroy associated microposts" do
-      microposts = @user.microposts.dup
-      @user.destroy
+      microposts = user.microposts.dup
+      user.destroy
       expect(microposts).not_to be_empty # safety check in case dup be removed.
 
       microposts.each do |micropost|
