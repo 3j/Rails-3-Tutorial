5a3f169bc51211187dbdc7b0bbb6fc34099a965f
David G Chaves <davidgchaves@gmail.com>
2012-10-26 10:39:54 +0200

    Add stats, a follow form and follow/unfollow buttons

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,6 +43,17 @@ describe "Static pages" do
           page.should have_selector "li##{micropost.id}", text: micropost.content
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create :user }
+        before do
+          other_user.follow! user
+          visit root_path
+        end
+
+        it { should have_link "0 following", href: following_user_path(user) }
+        it { should have_link "1 followers", href: followers_user_path(user) }
+      end
     end
   end
 
