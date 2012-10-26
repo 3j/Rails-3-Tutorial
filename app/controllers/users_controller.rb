9f6ea51c5232e4728a1227265aa8106a2bfd4a32
David G Chaves <davidgchaves@gmail.com>
2012-10-26 12:01:23 +0200

    Add following and followers pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit , :update]
+  before_filter :signed_in_user,
+                only: [:index, :edit, :update, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   before_filter :registered_user, only: [:new, :create]
@@ -45,6 +46,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find params[:id]
+    @users = @user.followed_users.paginate page: params[:page]
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find params[:id]
+    @users = @user.followers.paginate page: params[:page]
+    render 'show_follow'
   end
 
   private
